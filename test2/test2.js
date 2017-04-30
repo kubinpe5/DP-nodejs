a87cf25d04c3a1a96060e97afddcc1a782a95305
Petr Kubín <1pkubin@seznam.cz>
2017-04-30 11:55:39 +0200

    [SAVE]

--- test2/test2.js
+++ test2/test2.js
@@ -1,16 +1,31 @@
 // Originally from: https://benchmarkjs.com/
+var result = {
+  result: [],
+
+  addLine: function( line ) {
+    this.result.push( line + "\n");
+    return this;
+  },
+
+  getResult: function() {
+    return this.result;
+  }
+
+};
+
 document.getElementById("prerequisities").innerHTML = "Benchmark.platform:";
-console.log("Benchmark.platform:");
+//console.log("Benchmark.platform:");
 
 for (let key in Benchmark.platform) {
     if (Benchmark.platform.hasOwnProperty(key)) {
-        console.log("    ", key, Benchmark.platform[key]);
+  //      console.log("    ", key, Benchmark.platform[key]);
     }
     if ( Benchmark.platform.hasOwnProperty(key) && 
     		key == "description" || key == "ua" || key == "" || key == "os" || 
     		key == "version" || key == "name" || key == "layout") {
     	var breakline = document.createElement("br");
         var node = document.createTextNode(key + ", " + Benchmark.platform[key]);
+        result.addLine(key + ", " + Benchmark.platform[key]);
         document.getElementById("description").appendChild(node);
         document.getElementById("description").appendChild(breakline);
     }
@@ -21,7 +36,8 @@ var innerDiv = document.createTextNode('Performance test to determine fastest is
 div.appendChild(document.createElement("br"));
 div.appendChild(innerDiv);
 
-console.log("\n Performance test to determine fastest is in progress; please wait...");
+//console.log("\n Performance test to determine fastest is in progress; please wait...");
+result.addLine("\n Performance test to determine fastest is in progress; please wait...");
 
 var suite = new Benchmark.Suite;
 
@@ -89,13 +105,17 @@ suite.add('For loop, basic', function() {
   var breakline = document.createElement("br");
   document.getElementById("test_case").appendChild(obj);
   document.getElementById("test_case").appendChild(breakline);
-  console.log(String(event.target), event);
+  //console.log(String(event.target), event);
+  result.addLine(String(event.target), event);
 })
 .on('complete', function() {
   document.getElementById("result").innerHTML = 'Fastest is ' + this.filter('fastest').map('name') + "</br>";
   document.getElementById("result").innerHTML += 'Slowest is ' + this.filter('slowest').map('name');
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-  console.log('Fastest is ' + this.filter('slowest').map('name'));
+  //console.log('Fastest is ' + this.filter('fastest').map('name'));
+  //console.log('Fastest is ' + this.filter('slowest').map('name'));
+  result.addLine('Fastest is ' + this.filter('fastest').map('name'));
+  result.addLine('Slowest is ' + this.filter('slowest').map('name'));
+  console.log(result.getResult());
 })
 // run async
 .run({ 'async': true });
