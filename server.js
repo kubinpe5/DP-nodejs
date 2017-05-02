fa26445e97819644b03c6a1463c3ea9bcc5f40ad
Petr Kubín <1pkubin@seznam.cz>
2017-05-02 21:59:13 +0200

    [SAVE]

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-var finalhandler = require('finalhandler');
-var http = require('http');
-var serveStatic = require('serve-static');
+var express = require('express');
+var app = express();
+var axios = require("axios");
+var open = require("open");
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
-// Serve up public/ftp folder
-var serve = serveStatic('./', {'index': 'index.html'});
+app.use(express.static('./'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Create server
-var server = http.createServer(function (req, res) {
-	serve(req, res, finalhandler(req, res));
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
 });
 
-// Listen
-server.listen(3000, function() {
-    console.log( 'Server has started and is listening on port 3000' );
+app.post('/autotest2', function(req, res) {
+	console.log("Post request");
+	open('http://localhost:3000/tests/test2.html', 'firefox');
+	res.redirect("/");
+})
+
+app.post('/results', function(req, res, next) {
+	console.log("Posláno na /results");
+	//console.log(req.body.results.result);
+	var htmlResponse = "";
+	for ( var i = 0; i < req.body.results.result.length; i++ ) {
+		htmlResponse += "<li>" + req.body.results.result[i] + "</li>";
+	}
+	res.send(htmlResponse);
+	//axios.get('/results');
+	//res.send('results ' + req.params.results);
+});
+
+app.listen(3000, function () {
+  console.log( 'Server has started and is listening on port 3000' );
 });
